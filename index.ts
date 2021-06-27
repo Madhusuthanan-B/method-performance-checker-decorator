43ffabcc0af51286bc8576d241fa15853d8489dd
Madhusuthanan-B <madhub081011@gmail.com>
2021-06-27 23:53:54 +0530

    2 files modified

--- index.ts
+++ index.ts
@@ -1,25 +1,45 @@
 import { EvaluatePerformance } from './performance-checker';
+import * as _ from 'underscore';
+
+const rawData = [
+  { company: 'facebeook', name: 'Steve', id: 1988 },
+  { company: 'microsoft', name: 'Sathya', id: 8785 },
+  { company: 'facebeook', name: 'Rob', id: 1921 },
+  { company: 'facebeook', name: 'Smith', id: 1954 },
+  { company: 'google', name: 'Wills', id: 2021 },
+  { company: 'facebeook', name: 'Robin', id: 1991 },
+  { company: 'facebeook', name: 'Meridith', id: 1964 },
+  { company: 'google', name: 'Sundar', id: 2054 },
+  { company: 'google', name: 'Ajay', id: 2087 },
+  { company: 'facebeook', name: 'Bob', id: 1932 },
+  { company: 'microsoft', name: 'Bill', id: 8888 },
+  { company: 'google', name: 'Faf', id: 2014 },
+  { company: 'microsoft', name: 'Rocky', id: 8000 }
+];
 
 class PerformanceTest {
   constructor() {}
 
   @EvaluatePerformance({
-    resultColor: 'red'
+    resultColor: 'yellow'
   })
-  longRunner() {
-    for (let i = 0; i < 200; i++) {
-      const fruits = ['Banana', 'Orange', 'Apple', 'Mango'];
-      fruits.sort();
-    }
+  groupWithPlainJavaScript() {
+    const result = rawData.reduce((group, current) => {
+      if (group[current.company]) {
+        group[current.company].push(current);
+      } else {
+        group[current.company] = [current];
+      }
+      return group;
+    }, {});
+    return result;
   }
 
   @EvaluatePerformance({
-    resultColor: 'yellow'
+    resultColor: 'red'
   })
-  testMethod() {
-    for (let i = 0; i < 500; i++) {
-      const fruits = ['Banana', 'Orange', 'Apple', 'Mango', 'a', 'b', 'c'];
-      fruits.sort();
-    }
+  groupWithUnderscore() {
+    const result = _.groupBy(rawData, 'company');
+    return result;
   }
 }
